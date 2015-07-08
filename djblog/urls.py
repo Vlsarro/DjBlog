9a2d368d76c284fc92f416a8b5c2e59fb3752e14
Vlsarro <drekmaro@gmail.com>
2015-07-08 18:28:36 +0300

    DjBlog created

---
+++ djblog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+]
